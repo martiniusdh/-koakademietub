1ff6cefe43b0722da8c415a64cd1cb44118cf075
martiniusdh <martiniusdimmen07@gmail.com>
2026-02-06 12:04:33 +0100

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,14 +1,14 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "DIN-API-KEY",
-  authDomain: "DITT-PROSJEKT.firebaseapp.com",
-  projectId: "DITT-PROSJEKT",
-  storageBucket: "DITT-PROSJEKT.appspot.com",
-  messagingSenderId: "ID",
-  appId: "APP-ID"
+  apiKey: "AIzaSyALF7_i6p9B_ZSZwghKELc0MxFvclvJnRk",
+  authDomain: "okonomiakademiet-f38e9.firebaseapp.com",
+  projectId: "okonomiakademiet-f38e9",
+  storageBucket: "okonomiakademiet-f38e9.firebasestorage.app",
+  messagingSenderId: "501791489585",
+  appId: "1:501791489585:web:d540585d078728c95c9f10",
+  measurementId: "G-6PNR2VFGXR"
 };
 
 const app = initializeApp(firebaseConfig);
