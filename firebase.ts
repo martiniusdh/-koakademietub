34e920a910a30de1b97c3b3ac5aa370870db0cae
martiniusdh <martiniusdimmen07@gmail.com>
2026-02-06 10:54:03 +0100

    Update and rename firebase.tsx to firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "DIN-API-KEY",
+  authDomain: "DITT-PROSJEKT.firebaseapp.com",
+  projectId: "DITT-PROSJEKT",
+  storageBucket: "DITT-PROSJEKT.appspot.com",
+  messagingSenderId: "ID",
+  appId: "APP-ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
