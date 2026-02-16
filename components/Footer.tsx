04b4b8cf294ff98d40711be0777ad4952228481e
martiniusdh <martiniusdimmen07@gmail.com>
2026-02-16 09:22:59 +0100

    fix: Remove bottom text from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,15 +19,7 @@ const Footer: React.FC = () => {
             <p>Skoleåret 2025/2026</p>
           </div>
         </div>
-        <div className="mt-8 border-t border-slate-800 pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
-          <p className="text-xs text-slate-500">
-            &copy; 2024 Økonomiakademiet UB. Med støtte fra Ungt Entreprenørskap.
-          </p>
-          <div className="flex gap-4 text-xs text-slate-500 underline">
-            <a href="#">Personvern</a>
-            <a href="#">Brukervilkår</a>
-          </div>
-        </div>
+
       </div>
     </footer>
   );
